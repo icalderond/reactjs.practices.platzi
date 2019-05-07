9bb082d764212d7cddf84fee0e3aa8164088b3d8
Israel Calderon de la Cruz <icalderond@outlook.com>
2019-05-07 17:41:59 -0500

    Using jsx

--- src/index.js
+++ src/index.js
@@ -8,8 +8,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const sum = (a,b) => a + b;
+
+var nombre = 'Jorge'
+
 // JSX
-const element = <h1>Hello, Platzi </h1>
+// const jsxi = <h1>Hello, Platzi </h1>;
+// const element = React.createElement('a',{href:'https://platzi.com'},'Hello, Platzi')
+
+// const element =<h1>Esto es una variable {nombre}.
+// <br/> Esta es una funcion ({sum(3,8)})
+// <br/> Esto es undefined ({undefined}) ({null}) </h1>;
+
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1',{},'Soy Israel'),
+    React.createElement('p',{},'Soy Ingeniero')
+)
+
+// const element = React.createElement(
+//     'h1',{},`Hola soy ${nombre}`
+// )
+
 const container = document.getElementById('app')
 
 // ( __que__ , __donde__ )
